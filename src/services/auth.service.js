a49ab60f38ba00ca6b58a7722598ec984c33d21b
pascualmanuel <manuelnpascual@gmail.com>
2023-02-02 18:06:17 -0300

    Auth

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 
-class AuthService {
-  constructor() {
-    this.app = axios.create({
-      baseURL: `${process.env.REACT_APP_BASE_URL}/auth`,
-      withCredentials: true,
-    });
-  }
-
+// class AuthService {
+//   constructor() {
+//     this.app = axios.create({
+//       baseURL: `${process.env.REACT_APP_BASE_URL}/auth`,
+//       withCredentials: true,
+//     });
+//   }
+  class AuthService {
+    constructor() {
+      this.app = axios.create({
+        baseURL: `footicket-server.onrender.com/auth`,
+        withCredentials: true,
+      });
+    }
   signup = (username, password, email, team_id) =>
     this.app.post("/signup", {username, password, email, team_id});
   login = (username, password) => this.app.post("/login", {username, password});
